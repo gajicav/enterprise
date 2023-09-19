05676093872b9be7a3fb31968b618c9a7731de50
Alexander <gajicav@mail.uc.edu>
2023-09-19 14:37:59 -0400

    Added controllers

--- src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
+++ src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -1,18 +1,60 @@
 package com.myplantdiary.enterprise;
 
+
+import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PlantDiaryController {
 
     /**
      * Handle the root (/) endpoint and return a start page
+     *
      * @return
      */
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+
+
         return "start";
+
     }
+
+    /**
+     * Endpoint to fetch specimens
+     */
+@GetMapping("/specimen")
+    public ResponseEntity fetchAllSpecimens(){
+    return new ResponseEntity(HttpStatus.OK);
+}
+
+    /**
+     * Endpoint to fetch specimen by ID
+     */
+@GetMapping("/specimen/{id}")
+    public ResponseEntity fetchSpecimenById(@PathVariable("id") String id){
+    return new ResponseEntity(HttpStatus.OK);
+}
+
+    /**
+     * Endpoint to create specimen
+     */
+@PostMapping(value = "/specimen", consumes = "application/json", produces = "application/json")
+    public Specimen createSpecimen(@RequestBody Specimen specimen){
+    return specimen;
+}
+
+    /**
+     * Endpoint to delete specimen by ID
+     */
+@DeleteMapping("/specimen/{id}")
+    public ResponseEntity deleteSpecimen(@PathVariable("id") String id){
+    return new ResponseEntity(HttpStatus.OK);
+}
+
 }
---
+++ src/main/java/com/myplantdiary/enterprise/dto/Specimen.java
@@ -0,0 +1,14 @@
+package com.myplantdiary.enterprise.dto;
+
+import lombok.Data;
+
+public @Data class Specimen {
+
+
+
+    private int PlantId;
+    private String specimenId;
+    private String latitude;
+    private String longitude;
+    private String description;
+}
